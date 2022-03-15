9493b49fe76e8913bdb05d4a8306deb7dadf30ac
TiagoMerc <tmrrosario9@gmail.com>
2022-03-15 17:41:16 -0300

    Sript para comparar dois números

---
+++ script.js
@@ -0,0 +1,47 @@
+/* function compararNumeros(num1, num2) {
+  const saoIguais = num1 === num2;
+  const soma = num1 + num2;
+
+
+} */
+
+function comparaNumeros(num1, num2) {
+  //Verificar números
+  if (!num1 || !num2) return 'Defina dois números!';
+
+  const primeiraFrase = criarPrimeiraFrase (num1, num2);
+  const segundaFrase = criaSegundaFrase(num1, num2);
+
+  return `${primeiraFrase} ${segundaFrase}`
+}
+
+function criarPrimeiraFrase(num1, num2) {
+  let saoIguais = '';
+
+  if(num1 !== num2) {
+    saoIguais = 'não';
+  }
+
+  return `Os números ${num1} e ${num2} ${saoIguais}são iguais.`
+};
+
+function criaSegundaFrase(num1, num2) {
+  const soma = num1 + num2;
+
+  let resultado10 = 'menor';
+  let resultado20 = 'menor';
+
+  const compara10 = soma > 10;
+  const compara20 = soma > 20;
+
+  if(compara10) {
+    resultado10 = 'maior'
+  }
+
+  if(compara20) {
+    resultado20 = 'maior';
+  }
+
+  return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.` 
+}
+console.log(comparaNumeros(1, 1));
